4c0c541d0e8aa16d00a1206c7f16590eb1e5a3c3
justIN-collab <63333679+justIN-collab@users.noreply.github.com>
2024-03-05 11:10:43 +0700

    Update hello-world.py

--- hello-world.py
+++ hello-world.py
@@ -1,9 +1,13 @@
-import streamlit as st
 import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
+import streamlit as st
+
 # Load data
-data_hari = pd.read_csv('dayy.csv')
+@st.cache
+def load_data():
+    data_hari = pd.read_csv('dayy.csv')
+    return data_hari
+
+data_hari = load_data()
 
 # Konversi kolom 'date' ke tipe data datetime
 data_hari['date'] = pd.to_datetime(data_hari['date'])
@@ -18,48 +22,11 @@ total_bulan = data_hari.groupby(['year', 'month'])[['casual', 'registered', 'cnt
 # Menghitung total pengguna per tahun
 total_tahun = data_hari.groupby('year')[['casual', 'registered', 'cnt']].sum()
 
-# Menghitung rata-rata pengguna per hari
-rata_rata = data_hari['cnt'].mean()
-
-# Create a DataFrame with a single row containing the average value
-rata_rata_df = pd.DataFrame({'Rata-rata Pengguna per Hari': [rata_rata]})
-
-# Membuat chart line untuk total pengguna per bulan
-plt.figure(figsize=(10, 6))
-for col in total_bulan.columns:
-    bulan_tahun = total_bulan.index.map(lambda x: f"{x[0]}-{x[1]:02}")  
-    plt.plot(bulan_tahun, total_bulan[col], label=col)
-
-st.subheader('Hasil olah data total penggunaan perbulan')
-
-plt.title('Total Pengguna per Bulan')
-plt.xlabel('Tanggal')
-plt.ylabel('Total Pengguna')
-plt.legend()
-plt.grid(True)
-plt.xticks(rotation=45)
-plt.tight_layout()
-st.pyplot(plt)
-with st.expander("See explanation"):
-    st.write(
-        """Dari data diatas menemukan jumlah penyewaan paling tinggi berada pada bulan 09 tahun 2012, lalu nomor dua tertinggi berada pada bulan 08 tahun 2012 sedangkan untuk hasil terendah berada dalam bulan 01 tahun 2011
-        """
-    )
-
-st.subheader('Rata-rata Pengguna per Tahun')
-# Membuat chart line untuk total pengguna per bulan
+# Menampilkan chart line untuk total pengguna per bulan
 for col in total_bulan.columns:
     bulan_tahun = total_bulan.index.map(lambda x: f"{x[0]}-{x[1]:02}")  
     st.line_chart(total_bulan[col], use_container_width=True)
-    st.pyplot()  # Gunakan st.pyplot() setelah setiap chart jika Anda tetap ingin menggunakan Matplotlib
 
-
-# Membuat chart bar untuk rata-rata pengguna per hari
-st.subheader('Rata-rata Pengguna per Hari')
-st.bar_chart(rata_rata_df)
-with st.expander("See explanation"):
-    st.write(
-        """Dari data table diatas disimpulkan bahwa rata rata pengguna perharinya berada pada angka sekitar 4.500
-        """
-    )
-
+# Menampilkan chart line untuk total pengguna per tahun
+for col in total_tahun.columns:
+    st.line_chart(total_tahun[col], use_container_width=True)
